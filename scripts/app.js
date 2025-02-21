223915e43f62d22e8cb7eaa8b55ed8db34075ec1
BALAKRAM TUDU <48530348+balakram@users.noreply.github.com>
2025-02-22 02:19:53 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
 /*// Firebase Configuration
 const firebaseConfig = {
     apiKey: "YOUR_FIREBASE_KEY",
